741098d3266efda02209f0a0dcad48b70f5a423b
Geysa Ribeiro <geysa.ribeirotoppan@gmail.com>
2025-10-08 08:49:28 -0300

    Arquivos iniciais para deploy

---
+++ src/index.js
@@ -0,0 +1,8 @@
+﻿
+function multiplicar(a, b) {
+  return a * b;
+}
+const app = require('express')();
+app.get('/', (req, res) => res.send('App rodando com CI/CD!'));
+
+module.exports = { multiplicar, app };
---
+++ tests/index.test.js
@@ -0,0 +1,10 @@
+﻿
+const { multiplicar } = require('../src/index');
+
+test('deve multiplicar 2 * 2 e retornar 4', () => {
+  expect(multiplicar(2, 2)).toBe(4);
+});
+
+test('deve multiplicar 5 * 2 e retornar 10', () => {
+  expect(multiplicar(5, 2)).toBe(10);
+});
