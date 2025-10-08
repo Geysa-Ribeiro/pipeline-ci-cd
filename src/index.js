fa87530238072ad7eba98affedbe6e39f4c4f2bf
Geysa Ribeiro <geysa.ribeirotoppan@gmail.com>
2025-10-08 09:23:25 -0300

    Corrigindo erro 500 no Vercel ajustando export do Express

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-﻿
-function multiplicar(a, b) {
-  return a * b;
-}
-const app = require('express')();
-app.get('/', (req, res) => res.send('App rodando com CI/CD!'));
+﻿// src/index.js - Versão ajustada para Vercel Serverless
 
-module.exports = { multiplicar, app };
+function soma(a, b) {
+  return a + b;
+}
+
+const express = require('express');
+const app = express(); // Inicia o Express
+
+// Define a rota principal
+app.get('/', (req, res) => {
+    res.send('App rodando com CI/CD! (ERRO 500 CORRIGIDO)');
+});
+
+// *EXPORTE APENAS O APP* para que o Vercel possa usá-lo como Serverless Function
+module.exports = app;
